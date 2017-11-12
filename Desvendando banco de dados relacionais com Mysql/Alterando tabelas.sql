c68da1432e088de63417005f00434f78cb0c603b
vandersonsilvapaim <33205433+vandersonsilvapaim@users.noreply.github.com>
2017-11-12 10:58:21 -0200

    Update Alterando tabelas.sql

--- Desvendando banco de dados relacionais com Mysql/Alterando tabelas.sql
+++ Desvendando banco de dados relacionais com Mysql/Alterando tabelas.sql
@@ -1,24 +1,19 @@
+CREATE SCHEMA my_schema ;
 
-CREATE SCHEMA my_shema ;
+CREATE TABLE my_schema.tabela_1 (a INTEGER, b CHAR(5));
 
-CREATE TABLE my_shema.table_1 ( a INTEGER , b CHAR(5) );
+ALTER TABLE my_schema.tabela_1 rename my_schema.pais;
 
-ALTER TABLE my_shema.table_1 RENAME my_shema.pais ;
+ALTER TABLE my_schema.pais CHANGE COLUMN a id INTEGER ;
 
-ALTER TABLE my_shema.pais CHANGE COLUMN a id INTEGER ;
+ALTER TABLE my_schema.pais MODIFY b VARCHAR(100);
 
-ALTER TABLE my_shema.pais MODIFY b VARCHAR(100);
+ALTER TABLE my_schema.pais MODIFY id INTEGER NOT NULL, CHANGE b nome_ptbr VARCHAR(100) NOT NULL;
+
+ALTER TABLE my_schema.pais ADD nome_eng VARCHAR(100) NOT NULL;
+
+ALTER TABLE my_schema.pais ADD nome_rus VARCHAR(100) NOT NULL AFTER nome_eng , ADD continente VARCHAR(100) NULL, ADD capital VARCHAR(100) NULL;
  
-ALTER TABLE my_shema.pais MODIFY id INTEGER NOT NULL ,
-                          CHANGE b nome_ptbr VARCHAR(100) NOT NULL ;
+ALTER TABLE my_schema.pais DROP COLUMN nome_rus;
 
-ALTER TABLE my_shema.pais ADD nome_eng VARCHAR(100) NOT NULL FIRST nome_ptbr ;
-
-ALTER TABLE my_shema.pais ADD nome_rus VARCHAR(100) NOT NULL AFTER nome_eng ,
-                          ADD continente VARCHAR(100) NULL , 
-                          ADD capital VARCHAR(100) NULL ;
-
-ALTER TABLE my_shema.pais DROP COLUMN nome_rus ;
-
-DESCRIBE my_shema.pais ;
-
+DESCRIBE my_schema.pais; 
